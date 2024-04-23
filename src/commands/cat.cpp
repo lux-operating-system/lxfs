a80435bd3db8bdec1d984bcd7ce8116c09e84b8f
jewelcodes <omarmelghoul01@gmail.com>
2024-04-22 23:33:32 -0400

    cat: cat command works

--- src/commands/cat.cpp
+++ src/commands/cat.cpp
@@ -19,12 +19,33 @@ int cat(int argc, char **argv) {
     int partition = stoi(argv[3]);
     string path = argv[4];
 
-    // find the directory
+    // find the directory entry
     LXFSDirectoryEntry *entry = new LXFSDirectoryEntry;
     if(!findEntry(disk, partition, path, entry)) {
         cerr << "path not found" << endl;
         return -1;
     }
-    
+
+    // read the file into memory - round up size according to the blocks
+    size_t blockSize = (entry->size + BLOCK_SIZE_BYTES - 1) / BLOCK_SIZE_BYTES;
+    blockSize++;
+    vector<uint8_t> data(blockSize*BLOCK_SIZE_BYTES);
+
+    // and follow the chain
+    uint64_t current = entry->block;
+    size_t index = 0;
+    while(current != LXFS_BLOCK_EOF) {
+        //cout << "read block " << dec << current << endl;
+        current = readNextBlock(disk, partition, current, data.data() + (index*BLOCK_SIZE_BYTES));
+        index++;
+    }
+
+    uint8_t *ptr = data.data() + BLOCK_SIZE_BYTES;
+
+    for(uint64_t i = 0; i < entry->size; i++) {
+        cout << ptr[i];
+    }
+
+    cout << endl;
     return 0;
 }
